86b355f4189454b3b49fe0c98a464af436e786ad
MalKurteshi <malkurteshi@gmail.com>
2018-10-15 19:50:19 +0200

    Initial commit

---
+++ vuevue1/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    
+  <b-navbar toggleable="md" type="dark" variant="info">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">Britecore</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav>
+      <b-nav-item href="#"> <router-link to="/">Home</router-link></b-nav-item>
+      <b-nav-item href="#"> <router-link to="/about">About</router-link></b-nav-item>
+      <b-nav-item href="#"> <router-link to="/policies">Policies</router-link></b-nav-item>
+      <b-nav-item href="#"> <router-link to="/register">Register</router-link></b-nav-item>
+     
+    </b-navbar-nav>
+
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+
+      <b-nav-form>
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-nav-form>
+
+     
+      <b-nav-item-dropdown right>
+        <!-- Using button-content slot -->
+        <template slot="button-content">
+          <em>User</em>
+        </template>
+        <b-dropdown-item href="#">Profile Settings</b-dropdown-item>
+        <b-dropdown-item href="#">Policy Settings</b-dropdown-item>       
+        <b-dropdown-item href="#">Signout</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
+  
+    <!--<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>-->
+    <router-view/>
+   
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuevue1/src/views/RiskDetails.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100" style="width: 75%">
+
+            <form class="wrap-input100">
+            <span class="login100-form-title">
+						Choose your risk type policy
+					</span>
+
+         
+
+            <div class="wrap-input100 validate-input"  data-validate = "Name is a required field">
+            <!--Qikjo mushet prej kur te klikon butonin perkates ne index.html-->    
+            Policy type:<input class="input100" type="text" name="policyType" placeholder="">
+            <span class="focus-input100"></span>
+            </div>
+            <div class="wrap-input100 validate-input" data-validate = "Surname is a required field">
+            <!--e zgjedh modelin e vetures-->    
+            Car Make:<input class="input100" type="text" name="Surname" placeholder="">
+            <span class="focus-input100"></span>
+            </div>
+            <!--START date dhe end date-->
+            <div class="wrap-input100 validate-input" data-validate = "Required!!">
+            Choose start date for this risk type: <input class="input100" type="date" id="startDate" name="RiskStart" placeholder="">
+            <span class="focus-input100"></span>
+            </div>
+            <div class="wrap-input100 validate-input" data-validate = "Required!!">
+            Choose end date for this risk type: <input class="input100" type="date" id="endDate" name="RiskEnd" placeholder="">
+            <span class="focus-input100"></span>
+            </div>
+        
+
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn">
+                       Get a quote
+                    </button>
+                </div>
+
+
+            </form>
+
+        </div>
+    </div>
+</div>
+
+</template>
