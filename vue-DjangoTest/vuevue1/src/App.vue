53364c7ba6a6529d3324489ac5ff06a7f2cad714
MalKurteshi <malkurteshi@gmail.com>
2018-10-18 19:38:13 +0200

    add

--- vue-DjangoTest/vuevue1/src/App.vue
+++ vue-DjangoTest/vuevue1/src/App.vue
@@ -1,61 +1,81 @@
 <template>
   <div id="app">
-    
-  <b-navbar toggleable="md" type="dark" variant="info">
 
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar toggleable="md" type="dark" variant="info" v-if="authenticated"><!--E SHTUME-->
 
-  <b-navbar-brand href="#">Britecore</b-navbar-brand>
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-  <b-collapse is-nav id="nav_collapse">
+      <b-navbar-brand href="#">Britecore</b-navbar-brand>
 
-    <b-navbar-nav>
-      <b-nav-item href="#"> <router-link to="/">Home</router-link></b-nav-item>
-      <b-nav-item href="#"> <router-link to="/about">About</router-link></b-nav-item>
-      <b-nav-item href="#"> <router-link to="/policies">Policies</router-link></b-nav-item>
-      <b-nav-item href="#"> <router-link to="/register">Register</router-link></b-nav-item>
+      <b-collapse is-nav id="nav_collapse">
 
-    </b-navbar-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#"> <router-link to="/">Home</router-link></b-nav-item>
+          <b-nav-item href="#"> <router-link to="/about">About</router-link></b-nav-item>
+          <b-nav-item href="#"> <router-link to="/policies">Policies</router-link></b-nav-item>
+          <b-nav-item href="#"> <router-link to="/register">Register</router-link></b-nav-item>
+          <b-nav-item href="#"> <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link><!--E SHTUME-->
+          </b-nav-item>
 
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto">
+        </b-navbar-nav>
 
-      <b-nav-form>
-        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-      </b-nav-form>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
 
-     
-      <b-nav-item-dropdown right>
-        <!-- Using button-content slot -->
-        <template slot="button-content">
-          <em>User</em>
-        </template>
-        <b-dropdown-item href="#">Profile Settings</b-dropdown-item>
-        <b-dropdown-item href="#">Policy Settings</b-dropdown-item>       
-        <b-dropdown-item href="#">Signout</b-dropdown-item>
-      </b-nav-item-dropdown>
-    </b-navbar-nav>
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
 
-  </b-collapse>
-</b-navbar>
-  
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile Settings</b-dropdown-item>
+            <b-dropdown-item href="#">Policy Settings</b-dropdown-item>
+            <b-dropdown-item href="#">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+
     <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>-->
-    <router-view/>
-   
+    <router-view  @authenticated="setAuthenticated"/><!--E SHTUME-->
+
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>//E SHTUME
+export default {
+    name: 'App',
+    data() {
+        return {
+            authenticated: false,
+            mockAccount: {
+                username: "nraboy",
+                password: "password"
+            }
+        }
+    },
+    mounted() {
+        if(!this.authenticated) {
+            this.$router.replace({ name: "login" });
+        }
+    },
+    methods: {
+        setAuthenticated(status) {
+            this.authenticated = status;
+        },
+        logout() {
+            this.authenticated = false;
+        }
+    }
 }
-</style>
+</script>
+
+
--- vue-DjangoTest/vuevue1/src/router.js
+++ vue-DjangoTest/vuevue1/src/router.js
@@ -1,49 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+//import login from "./views/login.vue"
+// import SecureComponent from "./views/secure.vue"
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
+        routes: [
+            {
+                path: '/',
+                name: Home,
+                redirect: {
+                    name: "login"
+                }
+            },
+            {
+                path: '/about',
+                name: 'about',
+
+                component: () => import('./views/About.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/policies',
-      name: 'policies',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Policies.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Register.vue')
-    },
-    {
-      path: '/riskDetails',
-      name: 'riskDetails',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/RiskDetails.vue')
-    }
+        path: '/policies',
+        name: 'policies',
+
+        component: () => import('./views/Policies.vue')
+},
+{
+    path: '/register',
+        name: 'register',
+
+    component: () => import('./views/Register.vue')
+},
+{
+    path: '/riskDetails',
+        name: 'riskDetails',
+
+    component: () => import('./views/RiskDetails.vue')
+},
+{
+    path: "/login",
+        name: "login",
+    // component: LoginComponent
+    component: () => import('./views/Login.vue')
+},
+{
+    path: "/secure",
+        name: "secure",
+    // component: SecureComponent
+    component: () => import('./views/Secure.vue')
+}
 
 
-  ]
+
+]
 })
